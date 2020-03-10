a64a53c85954de0612ee50e6edecb74fcc74c8c9
Lima Honorine <holi1900@student.miun.se>
2020-03-10 11:49:16 +0100

    Writes functions getNumber and addAnAlbum in the jukebox class.

--- Project/include/Jukebox.h
+++ Project/include/Jukebox.h
@@ -34,6 +34,8 @@ public:
      * receives an Album object by reference and adds to the albumList
      */
     void addAlbumToList();
+    // prompts user to enter choice and check if it is an integer and it returns the interger if not it prompts user to renter a number
+    int getNumber();
     /*
      * prints list of album names and promps user to choose and album, the choice is read as an interger, controlled
      * for that the choice is valid and then returns the index of the choosen album
--- Project/src/Jukebox.cpp
+++ Project/src/Jukebox.cpp
@@ -48,32 +48,71 @@ void Jukebox::writeAlbumListToFile()
 
 void Jukebox::addAlbumToList()
 {
-    Album pAlbum;
+    Album newAlbum;
+    string tmpString;
+    Song tmpSong;
+    int tmpInt = -1;
+    cout << "\nEnter the following data on the album you wish to add"<< endl;
+    cout<< "Album title: ";
+    getline(cin, tmpString);
+    auto it = find_if(albumList.begin(),albumList.end(), [tmpString] (const Album & pAlbum){return pAlbum.getAlbumName() == tmpString;} );
+    if (it != albumList.end())
+    {
+        cout << "\nThere is already an album with title name '" << tmpString << "' in the album list. Choose a unique name.";
+        addAlbumToList();
+    }
+    newAlbum.setAlbumName(tmpString);
+    cout<< "\nEnter the number of songs in the album";
+    tmpInt = getNumber();
+    for (int i=0; i < tmpInt; i++)
+    {
+        cout<< "\nenter the details of song number" << i+1<<endl;
+        cout<< "Song's title :";
+        getline(cin, tmpString);
+        tmpSong.setSongTitle(tmpString);
 
-    albumList.push_back(pAlbum);
+        cout<< "Song's artist :";
+        getline(cin, tmpString);
+        tmpSong.setArtist(tmpString);
+
+        cout<< "Song's length in seconds :";
+        tmpInt = getNumber();
+        tmpSong.setSongLength(tmpSong.getlengthFromInt(tmpInt));
+
+        newAlbum.addSongToList(tmpSong);
+    }
+
+    albumList.push_back(newAlbum);
 }
 
-int Jukebox::chooseAnAlbum()
+int Jukebox::getNumber()
 {
-    cout << "\nYour choice: ";
+    cout << ": ";
     int choice = 0;
     cin>>choice;
-    if (!cin)
+    if (!cin||choice<1)// This control ensures that no empty albums or song can be store in the list
     {
 
         do
         {
             cin.clear();
             cin.ignore(1000, '\n');
-            cout << "\nThe entry must be a number! your choice: ";
+            cout << "\nInvalid entry. Enter a positive integer greater than 0! Your choice: ";
             cin>>choice;
-        }while(!cin);
+        }while(!cin||choice<1);
     }
     cin.ignore(1000, '\n');
+    return choice;
+}
+
+int Jukebox::chooseAnAlbum()
+{
+    cout << "\nYour choice";
+    int choice = getNumber();
     int listSize= static_cast<int > (albumList.size());
     if (choice < 1 || choice > listSize)
     {
-        cout << "\nThe entry must be between 1 and " << listSize;
+        cout << "\nInvalid choice. Your album choice must lie between 1 and " << listSize;
         chooseAnAlbum();
     }
     return choice-1;// gets index by subtracting 1 from choice since choice is number from 1 and index from 0
@@ -172,5 +211,7 @@ void Jukebox::printAnAlbum()
     int myIndex = chooseAnAlbum();
     cout << albumList[myIndex]<< endl;
 }
+
+
 
 
